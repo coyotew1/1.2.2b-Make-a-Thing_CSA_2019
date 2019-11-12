302bbb75dbb83be26eb2d9812f72c856de3b6053
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-11-12 14:05:22 -0700

    inClass_1.1.4

--- Main.java
+++ Main.java
@@ -26,6 +26,7 @@ public class Main {
 	    System.out.println(teemo.getName());
         Thing HYDROFLASK = new Thing("Hydroflask","peach", "Cylinder");
         System.out.println(HYDROFLASK.getName());
+
         
     }
 }
