6e75ca341a2245bb9312aaa1c6bcbecc18b600e8
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-11-12 14:02:30 -0700

    inClass_1.1.3

--- Food.java
+++ Food.java
@@ -3,5 +3,20 @@
  */
 public class Food extends Thing 
 {
-    private string flavor()
+    private String flavor;
+    private int calories;
+
+    //Default Constructor
+    public Food()
+    {
+        
+    }
+
+    public Food(String color, String name, String shape, String flavor, int calories)
+    {
+        super(name, color, shape);
+
+        this.flavor = flavor;
+        this.calories = calories;
+    }
 }
