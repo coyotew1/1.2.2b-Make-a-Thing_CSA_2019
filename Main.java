836dd271788633aa4dd414980984bf142009f7c8
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-11-14 12:30:56 -0700

    1.1.5
    Instead of the massive disgusting blob of things, we put them in an array

--- Main.java
+++ Main.java
@@ -1,32 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        Thing rubiksCube = new Thing("Rubik's Cube", "multi-color", "cube");
-        System.out.println(rubiksCube.getName());
-        Thing duck = new Thing("Duck", "yellow", "large rubber duck");
-        System.out.println(duck.getName());
-        Thing hammer = new Thing("Hammer", "Brown", "like a hammer");
-        System.out.println(hammer.getName());
-        Thing aMysteriousUnnamedObject = new Thing("Unnamed", "all of the above", "all of the above");
-        System.out.println(aMysteriousUnnamedObject.getName());
-        Thing chair = new Thing("chair", "orange", "rounded");
-        System.out.println(chair.getName());
-        Thing dylan = new Thing("Dylan", "white", "human-shaped");
-        System.out.println(dylan.getName());
-        Thing golfClub = new Thing("Golf Club", "White", "ball");
-        System.out.println(golfClub.getName());
-        Thing human = new Thing("Bob", "orange", "square");
-        System.out.println(human.getName());
-        Thing wall = new Thing("wall", "white", "Long wall");
-        System.out.println(wall.getName());
-        Thing shovel = new Thing("Shovel", "Brown", "shovel-shaped");
-        System.out.println(shovel.getName());
-        Thing banana = new Thing("MONKEY FUEL", "yellow", "banana-shaped");
-        System.out.println(banana.getName());
-        Thing teemo = new Thing("Teemo", "Green, Brown", "Lil-Hooman shaped");
-	    System.out.println(teemo.getName());
-        Thing HYDROFLASK = new Thing("Hydroflask","peach", "Cylinder");
-        System.out.println(HYDROFLASK.getName());
 
+        Thing things = new {
+            Thing rubiksCube = new Thing("Rubik's Cube", "multi-color", "cube");
+            Thing duck = new Thing("Duck", "yellow", "large rubber duck");
+            Thing hammer = new Thing("Hammer", "Brown", "like a hammer");
+            Thing aMysteriousUnnamedObject = new Thing("Unnamed", "all of the above", "all of the above");
+            Thing chair = new Thing("chair", "orange", "rounded");
+            Thing dylan = new Thing("Dylan", "white", "human-shaped");
+            Thing golfClub = new Thing("Golf Club", "White", "ball");
+            Thing human = new Thing("Bob", "orange", "square");
+            Thing wall = new Thing("wall", "white", "Long wall");
+            Thing shovel = new Thing("Shovel", "Brown", "shovel-shaped");
+            Thing banana = new Thing("MONKEY FUEL", "yellow", "banana-shaped");
+            Thing teemo = new Thing("Teemo", "Green, Brown", "Lil-Hooman shaped");
+            Thing HYDROFLASK = new Thing("Hydroflask","peach", "Cylinder");
+        }
+
+        /*
+        System.out.println(rubiksCube.getName());
+        System.out.println(duck.getName());
+        System.out.println(hammer.getName());
+        
+        System.out.println(aMysteriousUnnamedObject.getName());
+        System.out.println(chair.getName());
+        System.out.println(dylan.getName());
+        System.out.println(golfClub.getName());
+        System.out.println(human.getName());
+        System.out.println(wall.getName());
+        System.out.println(shovel.getName());
+        System.out.println(banana.getName());
+	    System.out.println(teemo.getName());
+        System.out.println(HYDROFLASK.getName());
+        */
         
     }
 }
